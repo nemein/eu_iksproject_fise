76ecf6383839563ed69e5a7aee6849e5ca7270d0
Henri Bergius <henri.bergius@iki.fi>
2010-07-27 16:44:29 +0300

    Support all Midgard content objects

--- interface.php
+++ interface.php
@@ -16,16 +16,39 @@ class eu_iksproject_fise extends midgardmvc_core_component_baseclass
     public function inject_process()
     {
         // Subscribe to content changed signals from Midgard so we can send content to FISE for analysis
-        midgard_object_class::connect_default('midgardmvc_core_node', 'action-created', array('eu_iksproject_fise', 'store'), array());
-        midgard_object_class::connect_default('midgardmvc_core_node', 'action-updated', array('eu_iksproject_fise', 'store'), array());
+        $mgdschemas = midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
+        foreach ($mgdschemas as $mgdschema)
+        {
+            if (   $mgdschema == 'midgardmvc_core_login_session'
+                || $mgdschema == 'midgard_parameter')
+            {
+                continue;
+            }
+            midgard_object_class::connect_default($mgdschema, 'action-created', array('eu_iksproject_fise', 'store'), array());
+            midgard_object_class::connect_default($mgdschema, 'action-updated', array('eu_iksproject_fise', 'store'), array());
+        }
     }
 
-    public static function object_to_text($object)
+    public static function object_to_text(midgard_object $object)
     {
-        return strip_tags($object->content);
+        $text = '';
+        $props = get_object_vars($object);
+        $reflectionproperty = new midgard_reflection_property(get_class($object));
+        foreach ($props as $property => $value)
+        {
+            $type = $reflectionproperty->get_midgard_type($property);
+            switch ($type)
+            {
+                case MGD_TYPE_STRING:
+                case MGD_TYPE_LONGTEXT:
+                    $text .= strip_tags($object->content) . "\n\n";
+                    break;
+            }
+        }
+        return $text;
     }
 
-    public static function store($object, $params)
+    public static function store(midgard_object $object, $params)
     {
         $object_url = midgardmvc_core::get_instance()->configuration->fise_url . $object->guid;
         $ch = curl_init($object_url);
